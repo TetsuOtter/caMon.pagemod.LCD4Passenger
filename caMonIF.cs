9359f70c9ac039c734264207452964ae77e45aeb
ＴＲ <public@technotter.com>
2020-09-19 19:06:40 +0900

    Step20. Implement methods in IPages

--- caMonIF.cs
+++ caMonIF.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 
 namespace caMon.pagemod.LCD4Passenger
 {
 	public class caMonIF : caMon.IPages
 	{
+		public Page FrontPage { get; }
+
+		public event EventHandler BackToHome;
+		public event EventHandler CloseApp;
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
