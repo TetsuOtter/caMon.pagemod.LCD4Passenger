bb5c3e69977c1b4b98567edc7ca3d740861784c0
ＴＲ <public@technotter.com>
2020-09-23 01:53:08 +0900

    Create ConstantValues.cs
    定数値管理用ファイルを追加

---
+++ ConstantValues.cs
@@ -0,0 +1,10 @@
+﻿namespace caMon.pagemod.LCD4Passenger
+{
+	static public class ConstantValues
+	{
+		/// <summary>ハーフLCDなど, カットディスプレイタイプだと判定するアスペクト閾値</summary>
+		static public double IsCutLCD_AspectThreshold { get; } = 0.4;
+
+		
+	}
+}
